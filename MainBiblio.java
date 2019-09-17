1573a45cdf483f9ea4b453f622a88949d22dea12
IoanaNaicu <54493390+IoanaNaicu@users.noreply.github.com>
2019-09-17 17:22:08 +0300

    Update MainBiblio.java

--- MainBiblio.java
+++ MainBiblio.java
@@ -8,15 +8,17 @@ public class MainBiblio {
         List<Books> myBooks = new ArrayList<>();
 
         Biblioteca b1 = new Biblioteca(myAuthors, myBooks);
-        Author author1 = new Author("Creanga", "creanga@yahoo.com", "m", "07567899");
-        Author author2 = new Author("Mihai", "creanga@yahoo.com", "m", "07567899");
-        Author author3 = new Author("Ion", "creanga@yahoo.com", "m", "07567899");
-        Author author4 = new Author("Ioana", "creanga@yahoo.com", "m", "07567899");
+        Author author1 = new Author("Creanga", "aaaa@yahoo.com", "m", "07567899");
+        Author author2 = new Author("Mihai", "mihai@yahoo.com", "m", "07567899");
+        Author author3 = new Author("Ion", "ion@yahoo.com", "m", "07567899");
+        Author author4 = new Author("Ioana", "ioana@yahoo.com", "m", "07567899");
 
-        Books book1 = new Books("Caprele", 3, 3, author1);
-        Books book2 = new Books("Praslea", 5, 3, author1);
+        Books book1 = new Books("Caprele", 3, 10, author1);
+        Books book2 = new Books("Praslea", 5, 9, author1);
         Books book3 = new Books("Luceafarul", 3, 3, author2);
         Books book4 = new Books("Calul", 8, 3, author3);
+        Books book5 = new Books("Irinuca", 5, 5, author1);
+        Books book6 = new Books("HAha", 5, 10, author1);
         myAuthors.add(author1);
         myAuthors.add(author2);
         myAuthors.add(author3);
@@ -25,7 +27,8 @@ public class MainBiblio {
         myBooks.add(book2);
         myBooks.add(book3);
         myBooks.add(book4);
-
+        myBooks.add(book5);
+        myBooks.add(book6);
 
 
         Scanner in = new Scanner(System.in);
@@ -39,22 +42,20 @@ public class MainBiblio {
             Character input = in.next().charAt(0);
             switch (input) {
                 case 'a':
+                    //nu lasa sa introduci 2 autori cu acelasi e-mail
                     System.out.println("introdu nume autor:");
                     String name = in.next();
-                    while(!name.matches("^[a-zA-Z\\\\s]*$")){
+                    while (!name.matches("^[a-zA-Z\\\\s]*$")) {
                         System.out.println("Insert a valid name: ");
                         name = in.next();
                     }
 
                     System.out.println("introdu email autor:");
                     String email = in.next();
-                    while (!email.matches("^([\\w-.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$")) {
-                        System.out.println("Insert a valid e-mail");
+                    while (!email.matches("^([\\w-.]+){1,64}@([\\w&&[^_]]+){2,255}.[a-z]{2,}$") || b1.isAuthorDuplicate(email)) {
+                        System.out.println("E-mail is invalid or already exists. Insert another e-mail: ");
                         email = in.next();
-//            while (!isStudentDuplicate(studentEmail)) {
-//                System.out.println("Student already exists. Insert anoter student: ");
-//                studentEmail = in.next();
-//            }
+
                     }
 
                     System.out.println("introdu gen autor:");
@@ -66,33 +67,46 @@ public class MainBiblio {
 
                     System.out.println("introdu tel autor:");
                     String tel = in.next();
-                    while(!tel.matches("^\\+(?:[0-9] ?){6,14}[0-9]$")){
+                    while (!tel.matches("^\\+(?:[0-9] ?){6,14}[0-9]$")) {
                         System.out.println("Insert a valid phone numbers");
                         tel = in.next();
-
                     }
                     Author myAuthor = b1.createAuthor(name, email, gender, tel);
                     b1.addAuthors(myAuthor);
                     break;
+
                 case 'b':
                     b1.printAuthorsList();
                     break;
                 case 'c':
+                    //trb introdus un autor existent...daca inserezi un alt nume, cere sa mai introduci o data
                     System.out.println("Introduceti numele autorului care doresti sa il stergi din lista: ");
                     String authorToRemove = in.next();
+                    while(b1.returnAuthor(authorToRemove) == null){
+                        System.out.println("Introdu numele unui autor existent:");
+                        authorToRemove = in.next();
+                    }
                     b1.removeAuthors(authorToRemove);
                     break;
                 case 'd':
                     System.out.println("introdu nume carte:");
                     String name1 = in.next();
+
                     System.out.println("introdu numarul de pagini:");
                     int noOfPages = in.nextInt();
+
                     System.out.println("introdu rating-ul cartii:");
                     double rating = in.nextDouble();
+
+                    //trb introdus un autpr existent deja in lista. Daca inserezi altul, cere sa mai introduci o data
                     System.out.println("Autorii disponibili sunt mai jos. Alege un nume:");
                     b1.printAuthorsList();
                     String authorName = in.next();
-                    //verifica daca returnAuthor.name este null....daca e null sa introduci din nou numele
+                    while(b1.returnAuthor(authorName) == null){
+                        System.out.println("Introdu numele unui autor din lista afisata mai sus");
+                        authorName = in.next();
+                    }
+
                     Books myBook = b1.createBooks(name1, noOfPages, rating, b1.returnAuthor(authorName));
                     b1.addBooks(myBook);
                     break;
@@ -102,12 +116,16 @@ public class MainBiblio {
                 case 'f':
                     System.out.println("Introduceti numele cartii care doriti sa o stergi din lista: ");
                     String bookToRemove = in.next();
+                    while(!b1.findBook(bookToRemove)) {
+                        System.out.println("Carte inexistenta. Introdu o carte existenta in biblioteca: ");
+                        bookToRemove = in.next();
+                    }
                     b1.removeBooks(bookToRemove);
                     break;
                 case 'g':
                     System.out.println("Introduceti numele autorului ale carui carti vreti sa vizualizati ");
                     String nameAuthorSearchForBooks = in.next();
-                    System.out.println(b1.returnBooksAfterAuthor(nameAuthorSearchForBooks));
+                    b1.returnBooksAfterAuthor(nameAuthorSearchForBooks);
 
                     break;
                 case 'h':
@@ -125,7 +143,7 @@ public class MainBiblio {
 
     }
 
-    public static void printOptions(){
+    public static void printOptions() {
         System.out.println("Alege o optiune:");
         System.out.println("a) adauga autori");
         System.out.println("b) printeaza lista autorilor");
@@ -140,4 +158,3 @@ public class MainBiblio {
 
 
 }
-
